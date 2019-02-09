a7e34801904a84f9dc7e3eebac2190fc68da13b7
lulb8 <bonneville@et.esiea.fr>
2019-02-09 19:56:19 +0100

    Ajout de getter et setter

--- src/Buildings.java
+++ src/Buildings.java
@@ -5,21 +5,47 @@ public abstract class Buildings
     public String buildingPicturePath;
     public People people;
 
+
     /**
-     *
-     * @return String
+     * @return
      */
     public String getName() {
         return name;
     }
 
     /**
-     *
-     * @return String
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return
      */
     public String getBuildingPicturePath() {
         return buildingPicturePath;
     }
 
+    /**
+     * @param buildingPicturePath
+     */
+    public void setBuildingPicturePath(String buildingPicturePath) {
+        this.buildingPicturePath = buildingPicturePath;
+    }
+
+    /**
+     * @return
+     */
+    public People getPeople() {
+        return people;
+    }
+
+    /**
+     * @param people
+     */
+    public void setPeople(People people) {
+        this.people = people;
+    }
 }
 
