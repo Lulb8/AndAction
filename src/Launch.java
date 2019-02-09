a66ddd20ae31d7eb1d12e652d75a82f0603365a5
lulb8 <bonneville@et.esiea.fr>
2019-02-09 17:29:38 +0100

    modification de checkNumber (Launch)

---
+++ src/Launch.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Launch {
+
+    private boolean keepRunning = true;
+    Scanner scanner = new Scanner(System.in);
+
+    public void initialize(Studio studio, MenuBar menuBar){
+        System.out.println("Nom du studio : ");
+        String studioName = scanner.nextLine();
+        studio.setName(studioName);
+        /*System.out.println("Le nom du studio est : ");
+        System.out.println(studio.getName());*/
+
+
+        System.out.println("Budget du studio : ");
+        String budgetStudio = scanner.nextLine();
+        menuBar.setBudget(Double.parseDouble(budgetStudio));
+        /*System.out.println("Le budget du studio est : ");
+        System.out.println(menuBar.getBudget());*/
+    }
+
+    public void loop(){
+        while (keepRunning){
+            System.out.println("Que voulez vous faire?");
+            int choice = checkNumber();
+            this.launchAction(choice);
+        }
+    }
+
+    int checkNumber() {
+        int number = 0;
+        //String input = scanner.nextLine();
+        boolean isNumber = true;
+        while (isNumber) {
+            try {
+                number = Integer.parseInt(scanner.next());
+                isNumber = false;
+            } catch (NumberFormatException e) {
+                System.out.println("Vous devez entrer un nombre !");
+            }
+        }
+        return number;
+    }
+
+    public void launchAction(int choice){
+        switch (choice){
+            case 1:
+                System.out.println("1");break;
+            case 2:
+                System.out.println("2");break;
+            case 3:
+                System.out.println("3");break;
+            case 4:
+                System.out.println("4");break;
+            case 5:
+                System.out.println("5");break;
+            case 6:
+                this.quitApp();break;
+            default:
+                System.out.println("Ce batiment n'existe pas !");break;
+        }
+    }
+
+    void quitApp(){
+        System.out.println("Au revoir !");
+        keepRunning = false;
+    }
+}
