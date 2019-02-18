705c6ba1a5d7ec0bb0c6f4213303e3ead22c83ec
AmySian <44503383+AmySian@users.noreply.github.com>
2019-02-19 00:00:21 +0100

    Create ScriptwritersBuilding.java

---
+++ src/ScriptwritersBuilding.java
@@ -0,0 +1,22 @@
+public class ScriptwritersBuilding {
+
+    Scanner scan = new Scanner(System.in);
+    do {
+			System.out.println("Quelle est le titre de votre film ?");
+			String choice = scan.next();
+			System.out.println("Le titre est + choice);
+      }
+      
+
+    public ArrayList<Scriptwriter> InitializeScriptwriter(ArrayList<Scriptwriter> scriptwriters) {
+    System.out.println("Choisissez votre scénariste");
+        Scriptwriter a = new Scriptwriter(),b = new Scriptwriter(), c = new Scriptwriter();
+        a.createScriptwriter("Nom1",1000,"ACTION");
+        b.createScriptwriter("Nom2",500,"COMEDY");
+        c.createScriptwriter("Nom3",1100,"ADVENTURE")
+        Scriptwriters.add(a1);
+        Scriptwriters.add(a2);
+        Scriptwriters.add(a3);
+        return scriptwriters;
+    }
+}
