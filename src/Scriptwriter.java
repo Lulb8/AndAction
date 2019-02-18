67f5031abd81839ad89713c21f6f82d386028781
AmySian <44503383+AmySian@users.noreply.github.com>
2019-02-18 22:47:55 +0100

    Update Scriptwriter.java

--- src/Scriptwriter.java
+++ src/Scriptwriter.java
@@ -1,3 +1,16 @@
 public class Scriptwriter extends People
 {
-}
+public String theme;
+//il faut créer une lite de theme que le joueur choisira
+public String getTheme() {
+            return theme;
+        }
+    public Scriptwriter createScriptwriter(String name, double salary, String theme) {
+            Scriptwriter w = new Scriptwriter();
+            w.salary=salary;
+            w.theme=theme;
+            w.name=name;
+            return w;
+        }
+
+    }
