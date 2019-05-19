8329f0b0ed54e7b3bb96920ffc7f42df00ab1692
lulb8 <bonneville@et.esiea.fr>
2019-05-19 22:06:06 +0200

    Nettoyage

--- src/Console.java
+++ src/Console.java
@@ -5,8 +5,8 @@ public class Console {
     public static void main(String[] args) {
         Studio studio = new Studio();
         studio.moviesDone = new ArrayList<Movie>();
-        //MenuBar menuBar = new MenuBar();
-        //GameWindow gameWindow = new GameWindow();
+        //GameWindow gameWindow = new GameWindow(studio);
+
         Launch launch = new Launch();
         int state=-1; //état du film: 0 si le tournage n'a pas commencé, 1 si il est en tournage, 2 s'il est fini
 
